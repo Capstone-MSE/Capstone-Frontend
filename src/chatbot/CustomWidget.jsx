8229252e9b4ceb7f33d5c272954ba65af26032e0
SohnSW <irctivl@gmail.com>
2024-06-09 14:48:28 +0900

    이미지 확대해서보기 시작 전

---
+++ src/chatbot/CustomWidget.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CustomWidget = ({ imageurl }) => {
+  return (
+    <div>
+      <img
+        src={`data:image/png;base64,${imageurl}`}
+        alt="Chat Image"
+        style={{ maxWidth: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default CustomWidget;
--- src/components/UploadLearnfileButton.jsx
+++ src/components/UploadLearnfileButton.jsx
@@ -1,60 +1,7 @@
-// import React, { useState } from "react";
-
-// const UploadLearnfileButton = () => {
-//   const [files, setFiles] = useState([]);
-
-//   const handleFileChange = (event) => {
-//     const selectedFiles = event.target.files;
-//     const fileArray = Array.from(selectedFiles);
-//     setFiles(fileArray);
-//   };
-
-//   const handleUpload = async () => {
-//     const accessToken = localStorage.getItem("accessToken");
-//     const userID = localStorage.getItem("userID");
-//     for (let file of files) {
-//       const formData = new FormData();
-//       formData.append("file", file);
-
-//       try {
-//         console.log(`http://18.232.193.248:8080/${userID}/predict/photos`);
-//         const response = await fetch(
-//           `http://18.232.193.248:8080/${userID}/predict/photos`,
-//           {
-//             method: "POST",
-//             body: formData,
-//             headers: {
-//               Authorization: `Bearer ${accessToken}`,
-//             },
-//           }
-//         );
-
-//         if (response.ok) {
-//           console.log(`${file.name} uploaded successfully`);
-//         } else {
-//           alert(`Error uploading ${file.name}: ${response.statusText}`);
-//         }
-//       } catch (error) {
-//         console.error(`Error uploading ${file.name}:`, error);
-//       }
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <input type="file" multiple onChange={handleFileChange} />
-//       <button onClick={handleUpload}>Upload</button>
-//     </div>
-//   );
-// };
-
-// export default UploadLearnfileButton;
-
 import React, { useState, useRef } from "react";
-import styles from './UploadLearnfileButton.module.css';
+import styles from "./UploadLearnfileButton.module.css";
 
 const UploadLearnfileButton = () => {
-
   const fileInputRef = useRef(null);
 
   const [files, setFiles] = useState([]);
@@ -62,26 +9,19 @@ const UploadLearnfileButton = () => {
   const handleFileChange = (event) => {
     const selectedFiles = event.target.files;
     const fileArray = Array.from(selectedFiles);
-    console.log("Selected files:", fileArray); // 파일 목록을 콘솔에 출력
+    console.log("Selected files:", fileArray);
     setFiles(fileArray);
   };
 
   const handleUpload = async () => {
-
     const accessToken = localStorage.getItem("accessToken");
     const userID = localStorage.getItem("userID");
-
-    if (!accessToken || !userID) {
-      console.error("Access token or user ID is missing");
-      alert("Access token or user ID is missing");
-      return;
-    }
 
     for (let file of files) {
       const formData = new FormData();
       formData.append("file", file);
 
-      console.log(`Uploading ${file.name}...`); 
+      console.log(`Uploading ${file.name}...`);
 
       try {
         const response = await fetch(
@@ -89,7 +29,7 @@ const UploadLearnfileButton = () => {
           {
             method: "POST",
             body: formData,
-            mode: 'no-cors',
+            mode: "no-cors",
             headers: {
               Authorization: `Bearer ${accessToken}`,
             },
@@ -118,21 +58,23 @@ const UploadLearnfileButton = () => {
 
   return (
     <div>
-      <input type="file" ref={fileInputRef} style={{ display: 'none' }} multiple onChange={handleFileChange} />
-      <button className={styles.btn} onClick={openFileDialog}>파일선택</button>
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        multiple
+        onChange={handleFileChange}
+      />
+      <button className={styles.btn} onClick={openFileDialog}>
+        파일선택
+      </button>
       {files.length > 0 && (
         <>
-          {/*<div className={styles.fileList}>
-            {files.map((file, index) => (
-              <div key={index}>{file.name}</div>
-            ))}
-          </div>*/}
           <button className={styles.btn} onClick={handleUpload}>
             업로드
           </button>
         </>
       )}
-
     </div>
   );
 };
